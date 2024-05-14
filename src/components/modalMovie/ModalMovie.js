46acee66efd39ef4bd84ba34a92170db25952e9f
Nory9 <noormisk94@gmail.com>
2024-05-14 19:31:48 +0000

    post data to db but with error lol

--- src/components/modalMovie/ModalMovie.js
+++ src/components/modalMovie/ModalMovie.js
@@ -2,8 +2,35 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { Image } from 'react-bootstrap';
+import { useState } from 'react';
+import axios from 'axios';
 import './ModalMovie.css'
+
 function ModalMovie(props){
+  console.log('Clicked item:', props.clickedItem);
+
+  const [comment, setComment] = useState('');
+  const addToFav = async (movie) => {
+    console.log(movie);
+    const serverURL = `https://movies-library-6ldd.onrender.com/addMovie`;
+    const mov = {
+       id: movie.id,
+      title: movie.title,
+       genre: "",
+     overview: movie.overview,
+     release_date: movie.date,
+      poster: `https://image.tmdb.org/t/p/w500${movie.poster}`,      
+      comment: comment
+    };
+    console.log(movie)
+    // try {
+      const response = await axios.post(serverURL, mov);
+      console.log('Server response:', response.data);
+    // } catch (error) {
+    //   console.error('Failed to add to favorites:', error);
+    // }
+};
+
     return (
     <Modal
     show={props.show}
@@ -13,11 +40,21 @@ function ModalMovie(props){
       <Modal.Title>{props.clickedItem.title}</Modal.Title>
     </Modal.Header>
     {/* <Modal.Img variant="top" src={`https://image.tmdb.org/t/p/w500${props.clickedItem.poster}`} /> */}
+    
     <Modal.Body>
     <Image className='image' src={`https://image.tmdb.org/t/p/w500${props.clickedItem.poster}`}></Image>
+    <textarea
+          className="form-control mt-3"
+          placeholder="Add a comment..."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
      {/* {props.clickedItem.overview} */}
     </Modal.Body>
     <Modal.Footer>
+      <Button variant="secondary" onClick={()=>addToFav(props.clickedItem)}>
+      💗
+      </Button>
       <Button variant="secondary" onClick={props.handleClose}>
         Close
       </Button>
