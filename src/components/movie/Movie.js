0cf7b3a24299da15ffe019b66563ff177aaccf1e
Nory9 <noormisk94@gmail.com>
2024-05-14 11:30:02 +0000

    added modal

--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -1,20 +1,38 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import ModalMovie from '../modalMovie/ModalMovie';
+import { useState } from 'react';
 import './Movie.css'
 function Movie(props){
+  const [isExpanded, setIsExpanded] = useState(false); 
+  const [clickedItem, setClickedItem] = useState({});
+    const toggleDescription = () => setIsExpanded(!isExpanded);
+    const previewText = props.overview.split(' ').slice(0,7).join('\n');
+    const [show, setShow] = useState(false);
+// console.log(props)
+  const handleClose = () => {
+    setShow(false)
+    setClickedItem(props)
+  };
+  const handleShow = () => setShow(true);
+    // console.log("Preview Text:", previewText);
+    // console.log(" Text:", props.overview);
     return(
 
 <div className='card1'>
-<Card  classNmae="c" style={{ width: '18rem' ,backgroundColor: '#f3f3f3'}}>
+<Card  className="c" style={{ width: '18rem' ,backgroundColor: '#f3f3f3'}}>
       <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${props.poster}`} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>
-          {props.overview}
+        {isExpanded ? props.overview : previewText + ' ...'}
+          {/* {props.overview} */}
         </Card.Text>
-        <Button variant="primary">3</Button>
+        <Button variant="primary" className='btn' onClick={() => { handleShow(props)}}>Add to favorite</Button>
+        <Button className='btn' onClick={toggleDescription} variant="primary"> {isExpanded ? 'See Less' : 'See More'}</Button>
       </Card.Body>
     </Card>
+    <ModalMovie show={show} handleClose={handleClose} clickedItem={clickedItem}></ModalMovie>
     </div>
     );
 }
